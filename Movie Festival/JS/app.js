3117720f8791acddae7eb530a2f46ed3b210e9f2
Darko Sasa <shale45@gmail.com>
2019-03-11 15:10:22 +0100

    finished Movie Festival app

---
+++ Movie Festival/JS/app.js
@@ -0,0 +1,140 @@
+let moviesList = [];
+let programsList = [];
+let summerFestival = new Festival("Summer Festival");
+
+function createMovie() {
+
+  let titleElement = document.getElementById("movie-title");
+  let lengthElement = document.getElementById("movie-duration");
+  let genreElement = document.getElementById("genre");
+  let selectedGenreElement = genreElement[genreElement.selectedIndex];
+  let errorElement = document.getElementById("movie-error");
+  let selectMovie= document.getElementById("select-movie");
+  let movieListElement = document.getElementById("movies-list");
+
+  let title = titleElement.value;
+  let length = parseInt(lengthElement.value);
+  let genre = selectedGenreElement.value;
+
+  if(!title || !length || (genre === "")) {
+    errorElement.textContent = "Error! Please input movie title, length and genre";
+    return;
+  }
+
+  errorElement.textContent = "";
+  let movie = new Movie(title, length, genre);
+  moviesList.push(movie);
+
+  let listOfMovies = "<ol>";
+  let moviesOptions = "<option value=\"\">  --</option>";
+
+  for (let i = 0; i < moviesList.length; i++) {
+    let singleMovie = moviesList[i];
+    listOfMovies += "<li>" + singleMovie.getData() + "</li>";
+    moviesOptions += "<option value=\"" + i + "\">" + singleMovie.title + "</option>";
+  }
+  listOfMovies += "</ol>";
+  selectMovie.innerHTML = moviesOptions;
+  movieListElement.innerHTML = listOfMovies;
+
+  titleElement.value = "";
+  lengthElement.value = "";
+  genreElement.value = "";
+
+};
+
+
+function createProgram() {
+
+  let dateElement = document.getElementById("program-date");
+  let errorElement = document.getElementById("program-error");
+  let programListElement = document.getElementById("program-list");
+  let programSelectElement = document.getElementById("select-program");
+  let programToFestivalSelect = document.getElementById("program-to-festival");
+
+  let date = dateElement.value;
+  
+  if(!date) {
+    errorElement.textContent ="Error, Please select festival Date."
+    return;
+  }
+
+  let program = new Program(new Date(date));
+  programsList.push(program);
+
+  let listOfPrograms = "<ol>";
+  let programsOptions = "<option value='none'>  --</option>";
+
+  for (let i = 0; i < programsList.length; i++) {
+    let prog = programsList[i];
+    listOfPrograms += "<li>" + prog.getData() + "</li>";
+    programsOptions += "<option value='" + i + "'>" + prog.getData() + "</option>";
+  }
+  listOfPrograms += "</ol>";
+
+  programListElement.innerHTML = listOfPrograms;
+  programToFestivalSelect.innerHTML = programsOptions;
+  programSelectElement.innerHTML = programsOptions;
+
+};
+
+
+function addMovieToProgram() {
+
+  let movieSelectElement = document.getElementById("select-movie");
+  let programSelectElement = document.getElementById("select-program");
+  let errorElement = document.getElementById("add-movie-error");
+  let listOfMoviesInProgramElement = document.getElementById("program-list-of-movies");
+
+  let selectedMove = movieSelectElement[movieSelectElement.selectedIndex].value;
+  let selectedProgram = programSelectElement[programSelectElement.selectedIndex].value;
+
+  if(selectedMove === "" || selectedProgram == "") {
+    errorElement.textContent = "Error! Please select Movie & Program";
+    return;
+  }
+
+  errorElement.textContent = "";
+
+  programsList[selectedProgram].listOfMovies.push(moviesList[selectedMove]);
+  
+  let listOfMoviesInProgram = programsList[selectedProgram].listOfMovies;
+  let programOutput = "<p>" + programsList[selectedProgram].getData() + "</p>";
+  programOutput += "<ol>";
+
+  for (let i = 0; i < listOfMoviesInProgram.length; i++) {
+    let movieInProgram = listOfMoviesInProgram[i];
+    programOutput += "<li>" + movieInProgram.getData() + "</li>";
+  }
+  programOutput += "</ol>";
+
+  listOfMoviesInProgramElement.innerHTML = programOutput;
+
+};
+
+
+function addProgramToFestival() {
+
+  let addProgramToFestivalElement = document.getElementById("program-to-festival");
+  let errorFestivalElement = document.getElementById("program-festival-error");
+  let selectedProgram = addProgramToFestivalElement[addProgramToFestivalElement.selectedIndex].value;
+  let programAdded = document.getElementById("program-added");
+
+  if (selectedProgram == "") {
+    errorFestivalElement.textContent = "Error! Please select Program"; 
+    return;   
+  }
+  errorFestivalElement.textContent = "";
+  summerFestival.listOfPrograms.push(programsList[selectedProgram]);
+
+  programAdded.innerHTML = "Program added to Festival";
+};
+
+
+function showFestivalProgram() {
+  let programFestivalElement = document.getElementById("program-festival-list");
+
+  let showFestival = summerFestival.getData();
+  programFestivalElement.innerHTML = showFestival;
+  
+};
---
+++ Movie Festival/JS/entities.js
@@ -0,0 +1,71 @@
+function Movie(title, length, genre) {
+  this.title = title;
+  this.length = length;
+  this.genre = genre;
+};
+
+Movie.prototype.getData = function() {
+  let formattedGenre = this.genre.charAt(0) + this.genre.charAt(this.genre.length - 1);
+  let output = this.title + ", duration: " + this.length + " min, genre: " + formattedGenre.toUpperCase();
+  return output;
+};
+
+
+function Program(date) {
+  this.date = date;
+  this.listOfMovies = [];
+  this.totalNumberOfMovies = [];
+};
+
+Program.prototype.getData = function() {
+  let day = this.date.getDate();
+  let month = this.date.getMonth() + 1;
+  let year = this.date.getFullYear();
+  return "Program: " + day + "." + month + "." + year + ".";
+};
+
+
+function Festival(name) {
+  this.name = name;
+  this.listOfPrograms = [];
+};
+
+Festival.prototype.totalFestivalMovies = function() {
+  let sum = 0;
+
+  for(let i = 0; i < this.listOfPrograms.length; i++) {
+    let program = this.listOfPrograms[i];
+    sum += program.listOfMovies.length;
+  }
+  return sum;
+};
+
+Festival.prototype.getData = function() {
+  let output = "";
+  if(this.listOfPrograms.length < 1) {
+    output = "Festival Program will be anounced soon.";
+    return output;
+  }
+
+  output += "<p>Festival: " + this.name + ", Number of movies: " + this.totalFestivalMovies() + "</p>";
+  output += "<ol>";
+
+  for (let i = 0; i < this.listOfPrograms.length; i++) {
+    let program = this.listOfPrograms[i];
+    if(program.listOfMovies.length < 1) {
+      output += program.getData();
+      output += "There are no movies on the selected day.";
+    }
+    output += "<li>" + program.getData() + "</li>";
+    output += "<ol>";
+
+    for (let j = 0; j < program.listOfMovies.length; j++) {
+      let movie = program.listOfMovies[j];
+      output += "<li>" + movie.getData() + "</li>"; 
+    }
+    output += "</ol>";
+  }
+  output += "</ol>";
+
+  return output;
+};
